65cb197f69a8e7018353fb787cbb7e1c20135ac0
Roli <rroycehg@gmail.com>
2021-05-02 02:40:46 -0400

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,16 @@
+function introduction (name) {
+    return `Hi, my name is ${name}.`;
+}
+
+
+
+function introductionWithLanguage(name, language) {
+    console.log (`The two values are ${name} and ${language}.`);
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    console.log(`${name}`);
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
